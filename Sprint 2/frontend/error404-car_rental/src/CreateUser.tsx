c65e117e50bffbdfcdb1ba935dce46a5dffd3d5b
Alex <abelle2010@gmail.com>
2024-03-03 11:17:35 -0500

    Added svg for exiting create account, added more logic and passed data through as props to create a more personal expierence for the user

--- Sprint 2/frontend/error404-car_rental/src/CreateUser.tsx
+++ Sprint 2/frontend/error404-car_rental/src/CreateUser.tsx
@@ -1,192 +1,305 @@
-import React, { useEffect, useState } from 'react';
-import './styles/CreateUser.css';
-import NavBar from './NavBar.tsx';
-import db from '../../../backend/models/user.js'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./styles/CreateUser.css";
+import NavBar from "./NavBar.tsx";
+import axios from "axios";
 // @ts-ignore
-import { ReactComponent as UserSilhouette } from './svgs/userSilhouette.svg';
-export default function CreateUser(){
-    const [color, setColor] = useState("black");
-    const [fname,setFname]=useState("");
-    const [lname,setLname]=useState("");
-    const [email,setEmail]=useState("");
-    const [dob,setDob]=useState("");
-    const [license,setLicense]=useState("");
-    const [password,setPassword]=useState("");
-    const [accType,setAccType] = useState("rentee");
-    
+import { ReactComponent as UserSilhouette } from "./svgs/userSilhouette.svg";
+// @ts-ignore
+import { ReactComponent as CloseModal } from "./svgs/close-square.svg";
 
-    const [errorVisibility,setErrorVisibility] = useState({
-        name:false,
-        email:false,
-        dob:false,
-        license:false,
-        password:false,
-        status: false,
-    });
-    // False means that the errors will not show and true means they will show
+export default function CreateUser(props) {
+  const [color, setColor] = useState("black");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [dob, setDob] = useState("");
+  const [license, setLicense] = useState("");
+  const [password, setPassword] = useState("");
+  const [accType, setAccType] = useState("rentee");
 
-    const emailRegEx = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" // from chatGPT and tested
-    const passwordRegEx = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$"
-    const maxDob = 20091231;
-    const minDob = 19241231;
+  const [errorVisibility, setErrorVisibility] = useState({
+    name: false,
+    email: false,
+    dob: false,
+    license: false,
+    password: false,
+    status: false,
+  });
+  // False means that the errors will not show and true means they will show
 
+  const emailRegEx = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"; // from chatGPT and tested
+  const passwordRegEx =
+    "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$";
+  const maxDob = 20091231;
+  const minDob = 19241231;
 
+  function pageTitle() {
+    return <title>Create Account</title>;
+  }
 
-    function pageTitle(){
-        return(<title>Create Account</title>)
+  function colorsetter(event: any) {
+    const elementStyle = window.getComputedStyle(event.target);
+    setColor(elementStyle.backgroundColor);
+  }
+
+  function submitCheck() {
+    setErrorVisibility((errorVisibility) => ({
+      ...errorVisibility,
+      name: fname.length < 2 || lname.length < 2,
+    }));
+    //First name and Last name cannot be less than 2 letters
+    setErrorVisibility((errorVisibility) => ({
+      ...errorVisibility,
+      email: !RegExp(emailRegEx).test(email),
+    }));
+    let dobCompare = parseInt(dob.replace(/-/g, ""));
+
+    setErrorVisibility((errorVisibility) => ({
+      ...errorVisibility,
+      dob: dobCompare >= minDob && dobCompare <= maxDob && dob.length != 10,
+    }));
+    //dob must be at least 15 years ago date out of range but also check if it is at least 2009
+    setErrorVisibility((errorVisibility) => ({
+      ...errorVisibility,
+      password: !RegExp(passwordRegEx).test(password),
+    }));
+    setErrorVisibility((errorVisibility) => ({
+      ...errorVisibility,
+      license: license.length < 2,
+    }));
+
+    if (
+      !errorVisibility.name &&
+      !errorVisibility.password &&
+      !errorVisibility.email &&
+      !errorVisibility.dob &&
+      !errorVisibility.license
+    ) {
+      submit();
     }
+  }
 
-    function colorsetter(event : any){
-        const elementStyle = window.getComputedStyle(event.target);
-        setColor(elementStyle.backgroundColor);
-    }
+  function submit() {
+    const userData = {
+      fname: fname,
+      lname: lname,
+      email: email,
+      dob: dob,
+      licenseNum: license,
+      password: password,
+      accType: accType,
+    };
 
-    function submitCheck(){
-        setErrorVisibility(errorVisibility => ({...errorVisibility, name : fname.length < 2 || lname.length < 2}));
-        //First name and Last name cannot be less than 2 letters
-        setErrorVisibility(errorVisibility => ({...errorVisibility, email : !RegExp(emailRegEx).test(email)}));
-        let dobCompare = parseInt(dob.replace(/-/g,""));
+    let status: number = 0;
 
-        setErrorVisibility(errorVisibility => ({...errorVisibility, dob : ((dobCompare >= minDob && dobCompare <= maxDob) && dob.length != 10)}))
-        //dob must be at least 15 years ago date out of range but also check if it is at least 2009
-        setErrorVisibility(errorVisibility => ({...errorVisibility, password : !RegExp(passwordRegEx).test(password)}));
-        setErrorVisibility(errorVisibility => ({...errorVisibility, license : license.length < 2}));
+    axios
+      .post("http://localhost:8080/createUser", userData)
+      .then((res) => {
+        status = res.status;
 
-        if((!errorVisibility.name && !errorVisibility.password && !errorVisibility.email && !errorVisibility.dob && !errorVisibility.license )){
-            submit();
-        }
-    }
+        if (status == 200) {
+            setErrorVisibility((errorVisibility) => ({
+              ...errorVisibility,
+              status: true,
+            }));
 
-    function submit(){
-        const userData = {
-            fname: fname,
-            lname: lname,
-            email: email,
-            dob: dob,
-            licenseNum: license,
-            password: password,
-            accType: accType
-        };
+            //Setting props to be read in the previous page, NavBar.tsx
+            props.setUserInfo({
+              fname: fname,
+              lname: lname,
+              userColor: color,
+            });
+            props.setIsLoggedIn(true);
+            props.toggleModal();
+          } else {
+            setErrorVisibility((errorVisibility) => ({
+              ...errorVisibility,
+              status: false,
+            }));
+          }
 
-        let status : number = 0;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-        axios.post('http://localhost:8080/createUser', userData)
-        .then(res =>{
-            console.log(res.status)
-            status = res.status;
-        })
-        .catch(error=>{
-            console.error('Error:', error);
-        });
+  return (
+    <div className="createAccount">
+      {pageTitle()}
+      <NavBar pageTitle={document.title} />
+      <div className="mainContent">
+        <CloseModal className="close" onClick={props.toggleModal} />
+        <div className="leftContainer">
+          <div className="imageContainer">
+            <UserSilhouette className="UserSilhouette-svg" fill={color} />
+          </div>
 
-        if(status == 200){
-            setErrorVisibility(errorVisibility => ({...errorVisibility, status : true}));
-            
-        }
-        else{
-            setErrorVisibility(errorVisibility => ({...errorVisibility, status : false}));
-        }
-    }
-
-
-
-    return(
-        <div className='createAccount'>
-            {pageTitle()}
-            <NavBar pageTitle = {document.title}/>
-            <div className='mainContent'>
-                <div className='leftContainer'>
-                    <div className='imageContainer'>
-                       <UserSilhouette className="UserSilhouette-svg" fill = {color}/>
-                    </div>
-
-                    <div className='colorOptions'>
-                        <button className='colorButton' style = {{backgroundColor: 'blue'}} onClick={colorsetter}/>
-                        <button className='colorButton' style = {{backgroundColor: 'red'}} onClick={colorsetter}/>
-                        <button className='colorButton' style = {{backgroundColor: 'purple'}} onClick={colorsetter}/>
-                        <button className='colorButton' style = {{backgroundColor: 'green'}} onClick={colorsetter}/>
-                        <button className='colorButton' style = {{backgroundColor: 'pink'}} onClick={colorsetter}/>
-                        <button className='colorButton' style = {{backgroundColor: 'black'}} onClick={colorsetter}/>
-                    </div>
-
-                </div>
-                <div className='rightContainer'>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td className='fieldLabels'>First name</td>
-                                <td className='fieldInputs'>
-                                    <input type="text" placeholder='First Name' required autoFocus autoComplete="off" onChange={(e)=>setFname(e.target.value)}/>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td className='fieldLabels'>Last name</td>
-                                <td className='fieldInputs'>
-                                    <input type="text" placeholder='Last Name' required autoComplete="off" onChange={(e)=>setLname(e.target.value)}/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className='fieldLabels'>Email</td>
-                                <td className='fieldInputs'>
-                                    <input type="text " placeholder='Email' required autoComplete="on" onChange={(e)=>setEmail(e.target.value)}/>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td className='fieldLabels'>Date of birth</td>
-                                <td className='fieldInputs'>
-                                    <input type="date" placeholder='Date of Birth' required onChange={(e)=>setDob(e.target.value)} max="2009-12-31"/>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td className='fieldLabels'>License #</td>
-                                <td className='fieldInputs'>
-                                    <input type="test" placeholder='License #' required onChange={(e)=>setLicense(e.target.value)}/>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td className='fieldLabels'>Password</td>
-                                <td className='fieldInputs'>
-                                    <input type="password" placeholder='Password' required onChange={(e)=>setPassword(e.target.value)}/>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td className='fieldLabels'>Account Type</td>
-                                <td className='fieldInputs'>
-                                    <select className='fieldInputs' onChange={(e)=> {setAccType(e.target.value)}}>
-                                        <option value ="rentee">I want to rent a car</option>
-                                        <option value="renter">I want to rent my car</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <p className={errorVisibility.name?'errorVisible':'error'}> Verify that your first and last name exceede 2 characters</p>
-                    <p className={errorVisibility.email?'errorVisible':'error'}> Verify that your email is in the correct format</p>
-                    <p className={errorVisibility.dob?'errorVisible':'error'}> Verify that your date of birth is between 1924 and 2009 </p>
-                    <p className={errorVisibility.license?'errorVisible':'error'}> Verify that your lisence number is alphanumeric</p>
-                    <p className={errorVisibility.password?'errorVisible':'error'}> Verify that your password meets the conditons</p>
-                    
-                    <ul className={errorVisibility.password?'errorVisible':'error'}>
-                        <li>At least 2 uppercase letters</li>
-                        <li>At least 1 special character [!@#$?..] </li>
-                        <li>At least 2 digits</li>
-                        <li>At least 3 lowercase letters</li>
-                        <li>At least a length of 8</li>
-                    </ul>
-
-
-                    <button className='submitButton' onClick={submitCheck}>Submit</button>
-                    <p className={errorVisibility.status?'errorVisible':'error'}>There has been an error creating your account</p>
-                </div>
-            </div>
-
+          <div className="colorOptions">
+            <button
+              className="colorButton"
+              style={{ backgroundColor: "blue" }}
+              onClick={colorsetter}
+            />
+            <button
+              className="colorButton"
+              style={{ backgroundColor: "red" }}
+              onClick={colorsetter}
+            />
+            <button
+              className="colorButton"
+              style={{ backgroundColor: "purple" }}
+              onClick={colorsetter}
+            />
+            <button
+              className="colorButton"
+              style={{ backgroundColor: "green" }}
+              onClick={colorsetter}
+            />
+            <button
+              className="colorButton"
+              style={{ backgroundColor: "pink" }}
+              onClick={colorsetter}
+            />
+            <button
+              className="colorButton"
+              style={{ backgroundColor: "black" }}
+              onClick={colorsetter}
+            />
+          </div>
         </div>
-    )
+        <div className="rightContainer">
+          <table>
+            <tbody>
+              <tr>
+                <td className="fieldLabels">First name</td>
+                <td className="fieldInputs">
+                  <input
+                    type="text"
+                    placeholder="First Name"
+                    required
+                    autoFocus
+                    autoComplete="off"
+                    onChange={(e) => setFname(e.target.value)}
+                  />
+                </td>
+              </tr>
 
+              <tr>
+                <td className="fieldLabels">Last name</td>
+                <td className="fieldInputs">
+                  <input
+                    type="text"
+                    placeholder="Last Name"
+                    required
+                    autoComplete="off"
+                    onChange={(e) => setLname(e.target.value)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabels">Email</td>
+                <td className="fieldInputs">
+                  <input
+                    type="text "
+                    placeholder="Email"
+                    required
+                    autoComplete="on"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td className="fieldLabels">Date of birth</td>
+                <td className="fieldInputs">
+                  <input
+                    type="date"
+                    placeholder="Date of Birth"
+                    required
+                    onChange={(e) => setDob(e.target.value)}
+                    max="2009-12-31"
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td className="fieldLabels">License #</td>
+                <td className="fieldInputs">
+                  <input
+                    type="text"
+                    placeholder="License #"
+                    required
+                    onChange={(e) => setLicense(e.target.value)}
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td className="fieldLabels">Password</td>
+                <td className="fieldInputs">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td className="fieldLabels">Account Type</td>
+                <td className="fieldInputs">
+                  <select
+                    className="fieldInputs"
+                    onChange={(e) => {
+                      setAccType(e.target.value);
+                    }}
+                  >
+                    <option value="rentee">I want to rent a car</option>
+                    <option value="renter">I want to rent my car</option>
+                  </select>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p className={errorVisibility.name ? "errorVisible" : "error"}>
+            {" "}
+            Verify that your first and last name exceede 2 characters
+          </p>
+          <p className={errorVisibility.email ? "errorVisible" : "error"}>
+            {" "}
+            Verify that your email is in the correct format
+          </p>
+          <p className={errorVisibility.dob ? "errorVisible" : "error"}>
+            {" "}
+            Verify that your date of birth is between 1924 and 2009{" "}
+          </p>
+          <p className={errorVisibility.license ? "errorVisible" : "error"}>
+            {" "}
+            Verify that your lisence number is alphanumeric
+          </p>
+          <p className={errorVisibility.password ? "errorVisible" : "error"}>
+            {" "}
+            Verify that your password meets the conditons
+          </p>
+
+          <ul className={errorVisibility.password ? "errorVisible" : "error"}>
+            <li>At least 2 uppercase letters</li>
+            <li>At least 1 special character [!@#$?..] </li>
+            <li>At least 2 digits</li>
+            <li>At least 3 lowercase letters</li>
+            <li>At least a length of 8</li>
+          </ul>
+
+          <button className="submitButton" onClick={submitCheck}>
+            Submit
+          </button>
+          <p className={errorVisibility.status ? "errorVisible" : "error"}>
+            There has been an error creating your account
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- Sprint 2/frontend/error404-car_rental/src/NavBar.tsx
+++ Sprint 2/frontend/error404-car_rental/src/NavBar.tsx
@@ -1,75 +1,88 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './styles/NavBar.css'
-import CreateUser from './CreateUser.tsx';
-
+import "./styles/NavBar.css";
+import CreateUser from "./CreateUser.tsx";
+// @ts-ignore
+import { ReactComponent as UserSilhouette } from "./svgs/userSilhouette.svg";
 
 export default function NavBar(props) {
-    const [homeActive, sethomeActive] = useState(props.pageTitle === "Home" || document.title === "Home");
-    const [createAccountActive, setcreateAccountActive] = useState(props.pageTitle === "Create Account" || document.title === "Create Account");
-    const [modal, setModal] = useState(false);
+  const [homeActive, sethomeActive] = useState(
+    props.pageTitle === "Home" || document.title === "Home"
+  );
+  const [createAccountActive, setcreateAccountActive] = useState(
+    props.pageTitle === "Create Account" || document.title === "Create Account"
+  );
+  const [modal, setModal] = useState(false);
 
-    function toggleModal() {
-        setModal(!modal);
+  const [userInfo, setUserInfo] = useState({
+    fname: "",
+    lname: "",
+    userColor: "",
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function toggleModal() {
+      setModal(!modal);
+  }
+
+  useEffect(() => {
+    sethomeActive(props.pageTitle === "Home" || document.title === "Home");
+    setcreateAccountActive(
+      props.pageTitle === "Create Account" ||
+        document.title === "Create Account"
+    );
+
+    if (homeActive === createAccountActive) {
+      if (document.title === "Home") {
+        setcreateAccountActive(false);
+      } else if (document.title === "Create Account") {
+        sethomeActive(false);
+      }
     }
+  }, [props.pageTitle, document.title]);
 
-    useEffect(() => {
-        sethomeActive(props.pageTitle === "Home" || document.title === "Home");
-        setcreateAccountActive(props.pageTitle === "Create Account" || document.title === "Create Account");
+  return (
+    <>
+      <div className="navBar">
+        <ul>
+          <li className="navbarLeft" id={homeActive ? "active" : ""}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbarLeft">
+            <a>About</a>
+          </li>
 
-        if (homeActive === createAccountActive) {
-            if (document.title === "Home") {
-                setcreateAccountActive(false)
-            }
-
-            else if (document.title === "Create Account") {
-                sethomeActive(false);
-            }
-        }
-
-    }, [props.pageTitle, document.title]);
-
-
-    return (
-        <>
-            <div className='navBar'>
-                <ul >
-                    <li className='navbarLeft' id={homeActive ? "active" : ""}>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className='navbarLeft'>
-                        <a>
-                            About
-                        </a>
-                    </li>
-
-                    {/* <li className='centerNav'>
+          {/* <li className='centerNav'>
             {document.title}
         </li> */}
 
-                    <li className='navbarRight'>
-                        <a>
-                            Sign in
-                        </a>
-                    </li>
+          <li className="navbarRight" id={createAccountActive ? "active" : ""}>
+            <a onClick={toggleModal}>
+              <UserSilhouette
+                width="20px"
+                height="20px"
+                className="userIcon"
+                fill={userInfo.userColor}
+              />
+              {!isLoggedIn
+                ? "Create Account"
+                : "Welcome back " + userInfo.fname}
+            </a>
+          </li>
 
-                    <li className='navbarRight' id={createAccountActive ? "active" : ""}>
-                        {/* <Link to ="/createuser">Create Account</Link> */}
-                        <a onClick={toggleModal}>Create Account</a>
-                    </li>
-                </ul>
-            </div>
+          <li className="navbarRight">{!isLoggedIn && <a>Sign in</a>}</li>
+        </ul>
+      </div>
 
-            {modal &&
-            <>
-            <div className='overlay' onClick={toggleModal}/>
-            <div className='modal-content'>
-                <CreateUser/>
-            </div>
-            
-            </>
-            }
+      {modal && (
+        <>
+          <div className="overlay" onClick={toggleModal} />
+          <div className="modal-content">
+            <CreateUser toggleModal={toggleModal} setUserInfo={setUserInfo} setIsLoggedIn = {setIsLoggedIn}/>
+          </div>
         </>
-    )
-
+      )}
+    </>
+  );
 }
