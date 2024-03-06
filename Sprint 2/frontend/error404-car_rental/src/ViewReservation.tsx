4d2ae942471587662ba371f948d226901a152161
Alex <abelle2010@gmail.com>
2024-03-05 21:53:53 -0500

    Added SVGs, Added reusable component for the reservations (proof of concept untill theres actual data), fixed z-indexes

--- Sprint 2/frontend/error404-car_rental/src/ViewReservation.tsx
+++ Sprint 2/frontend/error404-car_rental/src/ViewReservation.tsx
@@ -1,15 +1,94 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/ViewReservation.css";
+import NavBar from './NavBar.tsx';
+// @ts-ignore
+import { ReactComponent as Delete } from "./svgs/delete.svg";
+// @ts-ignore
+import { ReactComponent as Modify } from "./svgs/edit.svg";
+import axios from "axios";
+import {getCookie} from './CookieManager.ts';
 
 export default function ViewReservation() {
+
+  const [reservations,setReservations]=useState([{
+    carId: "",
+    location: "",
+    reservationDate: "",
+    returnDate: "",
+    userId: "",
+    __v: 0,
+    _id : "",
+  }]); 
+
   function pageTitle() {
     return <title>This is the page title</title>;
   }
 
+  function Reservation({res}) {
+    return (
+      <div className="reservationContainer">
+        <img className="carImage" />
+        <div className="actionbar">
+          <button className="viewLabel">View</button> {/*onClick={viewReservationOnClick(res._id)} */}
+          <Modify className="editSVG" onClick={modifyReservationOnClick(res._id)}/>
+          <Delete fill="red" className="deleteSVG" onClick={deleteReservationOnClick(res._id)}/>
+        </div>
+      </div>
+    )
+  }
+
+  function deleteReservationOnClick(reservationID: String) {
+
+  }
+
+  function viewReservationOnClick(reservationID: String) {
+
+  }
+
+  function modifyReservationOnClick(reservationID: String) {
+
+  }
+
+  function loadAllReservations(){
+    const userId = getCookie("usreID");
+
+    // /http://localhost:8080/reservations:id 
+
+    axios
+      .get("http://localhost:8080/reservations")
+      .then((res) => {
+        setReservations(res.data)
+        console.log(reservations)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+  }
+
+  useEffect(() => {
+    loadAllReservations();
+
+  }, [])
+
   return (
-    <div>
+    <div className="viewReservationContainer">
+      <NavBar pageTitle={document.title} />
       {pageTitle()}
-      <h1> This is for viewing reservations</h1>
+      <h1>Current Reservations</h1>
+      {reservations.map(reservation =>
+        <Reservation key={reservation._id} res = {reservation} />
+      )}
+
+
+
+      <h1>Past Reservations</h1>
+
+      {reservations.map(reservation =>
+        <Reservation key={reservation._id} res = {reservation} />
+      )}
+
+
     </div>
   );
 }
