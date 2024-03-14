885c794b74e5aca7ec2126d4313cde67018c6133
george_ezzat <=>
2024-03-14 17:52:25 -0400

    found a small issue in modifyreservations.tsx

--- Sprint 2/frontend/error404-car_rental/src/ModifyReservartions.tsx
+++ Sprint 2/frontend/error404-car_rental/src/ModifyReservartions.tsx
@@ -79,8 +79,8 @@ export default function ModifyReservations() {
   
     const handleUpdate = () => {
         const updatedFields = {
-          userId: editableReservation.userID,
-          carId: editableReservation.carID,
+          userID: editableReservation.userID,
+          carID: editableReservation.carID,
           pickupDate: editableReservation.pickupDate,
           returnDate: editableReservation.returnDate,
           pickuplocation: editableReservation.pickuplocation,
